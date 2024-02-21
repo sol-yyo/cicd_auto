c82a5703682ba940d1cc57b1155fccfe8de8e390
unknown <chaitri.gudhka@plusinfosys.com>
2024-02-21 11:51:10 +0530

    Added a secret

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
 	}
 	addItem(e){
 		if(this._inputElement.value !== ""){
+			var PASSWORD = "12345"
 			var newItem = {
 				text: this._inputElement.value, 
 				key: Date.now()
