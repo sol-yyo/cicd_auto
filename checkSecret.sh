81d6514b3073fa979aba0ca2d5883b84f0e5b82e
unknown <chaitri.gudhka@plusinfosys.com>
2024-02-21 12:31:46 +0530

    Changed the secret script

--- checkSecret.sh
+++ checkSecret.sh
@@ -15,13 +15,15 @@ for EXTENSION in "${SOURCE_FILE_EXTENSIONS[@]}"; do
     while IFS= read -r LINE; do
         # Check if the line contains variable declaration
         if [[ "$LINE" =~ \b(var|let|const)\s+(${SENSITIVE_VARIABLES[@]})\b ]]; then
-            # Check if the variable is assigned a value directly, excluding process.env assignments
-            if ! [[ "$LINE" =~ \b(var|let|const)\s+(${SENSITIVE_VARIABLES[@]})\s*=\s*(process\.env\.(${SENSITIVE_VARIABLES[@]})|['\"]\w*['\"])\s* ]]; then
-                # Print files and lines containing sensitive variables
-                echo "Exposed sensitive variable found in ${EXTENSION} files:"
-                echo "$LINE"
-                # Set the flag to true if sensitive variables are found
-                EXPOSED_VARIABLES_FOUND=true
+            # Check if the variable is assigned a value directly and not to process.env
+            if ! [[ "$LINE" =~ \b(var|let|const)\s+(${SENSITIVE_VARIABLES[@]})\s*=\s*process\.env\.${SENSITIVE_VARIABLES[@]} ]]; then
+                if ! [[ "$LINE" =~ \b(var|let|const)\s+(${SENSITIVE_VARIABLES[@]})\s*=\s*['"']\w*['"'] ;]] then
+                    # Print files and lines containing sensitive variables
+                    echo "Exposed sensitive variable found in ${EXTENSION} files:"
+                    echo "$LINE"
+                    # Set the flag to true if sensitive variables are found
+                    EXPOSED_VARIABLES_FOUND=true
+                fi
             fi
         fi
     done < <(grep -rnw . --include="*.${EXTENSION}" -e "${SENSITIVE_VARIABLES[@]}" 2>/dev/null)
