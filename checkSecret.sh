1a7a46cf4fa65aff01b3da52bdcd89e1773c779e
unknown <chaitri.gudhka@plusinfosys.com>
2024-02-21 11:49:20 +0530

    Changed the secret script

--- checkSecret.sh
+++ checkSecret.sh
@@ -12,11 +12,12 @@ EXPOSED_VARIABLES_FOUND=false
 # Loop through each source file extension
 for EXTENSION in "${SOURCE_FILE_EXTENSIONS[@]}"; do
     # Find all files with the current extension and search for sensitive variables
-    FILES_WITH_SENSITIVE_VARIABLES=$(grep -rnw . -e "${SENSITIVE_VARIABLES[@]}" --include="*.${EXTENSION}" 2>/dev/null)
+    FILES_WITH_SENSITIVE_VARIABLES=$(grep -rnw . -e "${SENSITIVE_VARIABLES[@]}" --include="*.${EXTENSION}" 2>/dev/null | grep -E '\b(var|let|const)\s+\w+\s*=\s*["'\'']?(PASSWORD|SECRET|TOKEN)["'\'']?;')
     if [ -n "$FILES_WITH_SENSITIVE_VARIABLES" ]; then
         # Print files and lines containing sensitive variables
         echo "Exposed sensitive variables found in ${EXTENSION} files:"
         echo "$FILES_WITH_SENSITIVE_VARIABLES"
+        # Set the flag to true if sensitive variables are found
         EXPOSED_VARIABLES_FOUND=true
     fi
 done
